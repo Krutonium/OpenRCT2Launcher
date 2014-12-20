008811f719a91dcc741fded2abe52d84441d7063
Kenton <PFCKrutonium@gmail.com>
2014-12-20 03:13:14 -0500

    Checks Remote Against Local to determine if Update is available

--- Launcher/Launcher/Form1.Designer.vb
+++ Launcher/Launcher/Form1.Designer.vb
@@ -22,17 +22,49 @@ Partial Class frmLauncher
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrCheckIfDone = New System.Windows.Forms.Timer(Me.components)
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.cmdLaunchGame = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'tmrCheckIfDone
+        '
+        Me.tmrCheckIfDone.Enabled = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(12, 9)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(158, 13)
+        Me.lblStatus.TabIndex = 0
+        Me.lblStatus.Text = "Checking if you are up to date..."
+        '
+        'cmdLaunchGame
+        '
+        Me.cmdLaunchGame.Location = New System.Drawing.Point(15, 50)
+        Me.cmdLaunchGame.Name = "cmdLaunchGame"
+        Me.cmdLaunchGame.Size = New System.Drawing.Size(157, 59)
+        Me.cmdLaunchGame.TabIndex = 1
+        Me.cmdLaunchGame.Text = "Launch OpenRCT2"
+        Me.cmdLaunchGame.UseVisualStyleBackColor = True
         '
         'frmLauncher
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(430, 243)
+        Me.ClientSize = New System.Drawing.Size(484, 261)
+        Me.Controls.Add(Me.cmdLaunchGame)
+        Me.Controls.Add(Me.lblStatus)
         Me.Name = "frmLauncher"
         Me.Text = "OpenRCT2 Launcher"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents tmrCheckIfDone As System.Windows.Forms.Timer
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents cmdLaunchGame As System.Windows.Forms.Button
 
 End Class
--- Launcher/Launcher/Form1.vb
+++ Launcher/Launcher/Form1.vb
@@ -1,6 +1,60 @@
-﻿Public Class frmLauncher
-    Dim URL As String = "https://openrct2.com/download/latest" ' Should always give us the latest download ;)
-    Private Sub frmLauncher_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.IO
+Imports System.Net
+Imports System.Threading
+Imports Microsoft.Win32
 
+Public Class frmLauncher
+
+    Dim URL As String = "https://openrct2.com/download/latest" ' Should always give us the latest download ;)
+    Dim VerURL As String = "https://www.dropbox.com/s/9uk7qdb5s4coakn/OpenRCTV.txt?dl=1"
+    Dim RemoteVer As Integer
+    Dim LocalVer As Integer
+
+    Dim RemoteDone As Boolean = False
+    Dim LocalDone As Boolean = False
+
+    Private Sub frmLauncher_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim GetRemote = New Thread(AddressOf GetRemoteVer)
+        Dim GetLocal = New Thread(AddressOf GetLocalVer)
+        GetRemote.Start()
+        GetLocal.Start()
+    End Sub
+
+    Private Sub GetRemoteVer()
+        If My.Computer.Network.IsAvailable = True Then
+            Try 'If the computer has a network connection but no internet, we want to avoid crashing.
+                Dim WC As New WebClient
+                RemoteVer = WC.DownloadString(VerURL)
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+        RemoteDone = True
+    End Sub
+
+    Private Sub GetLocalVer()
+        Try
+            Dim Reg As RegistryKey = (Registry.CurrentUser.CreateSubKey("OpenRCT2Launcher"))
+            If Reg.GetValue("LocalVer") Is Nothing Then
+                Reg.SetValue("LocalVer", "0")
+                LocalVer = "0"
+            Else
+                LocalVer = Reg.GetValue("LocalVer")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        LocalDone = True
+    End Sub
+
+    Private Sub tmrCheckIfDone_Tick(sender As Object, e As EventArgs) Handles tmrCheckIfDone.Tick
+        If LocalDone = True And RemoteDone = True Then
+            tmrCheckIfDone.Enabled = False
+            If RemoteVer > LocalVer Then
+                lblStatus.Text = "Update is Available"
+            Else
+                lblStatus.Text = "Up to Date"
+            End If
+        End If
     End Sub
 End Class
