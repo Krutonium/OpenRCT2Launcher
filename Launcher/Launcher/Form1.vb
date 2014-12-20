5c7ce217c7972b017923d6528599d20a69318c2d
Kenton <PFCKrutonium@gmail.com>
2014-12-20 02:38:36 -0500

    Commit Launcher

---
+++ Launcher/Launcher/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmLauncher
+    Dim URL As String = "https://openrct2.com/download/latest" ' Should always give us the latest download ;)
+    Private Sub frmLauncher_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
