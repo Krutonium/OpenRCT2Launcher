ed3712d080495a5979af4d4ea9bbc49ee4bb63e1
PFCKrutonium <PFCKrutonium@gmail.com>
2015-02-02 20:15:08 -0500

    Moved Game to SubDirectory, Finished Update Check

--- Launcher/Launcher/Form1.vb
+++ Launcher/Launcher/Form1.vb
@@ -4,19 +4,19 @@ Imports System.Net
 Imports System.Threading
 Imports Microsoft.Win32
 Imports System.Text.RegularExpressions
+Imports System.Random
 
 Public Class frmLauncher
 
     Dim URL As String = "https://openrct2.com/download/latest" ' Need to replace with Download from Version Text
-    Dim VerURL As String = "https://www.dropbox.com/s/9uk7qdb5s4coakn/OpenRCTV.txt?dl=1"
-    Dim RemoteVer As Integer        'Will contain the version of OpenRCT2 from the server
-    Dim LocalVer As Integer         'Will contain the version of OpenRCT2 on this computer
+    Dim RemoteVer As String        'Will contain the version of OpenRCT2 from the server
+    Dim LocalVer As String         'Will contain the version of OpenRCT2 on this computer
 
     Dim RemoteDone As Boolean = False   'These two variables act as flags so that we know when both processes are complete.
     Dim LocalDone As Boolean = False
 
-    Dim OpenRCTEXEName As String = "openrct2.exe"   'So it's easy to change when/if it gets changed officially.
-    Dim OpenRCTDLLName As String = "openrct2.dll"
+    Dim OpenRCTEXEName As String = ".\OpenRCT2\openrct2.exe"   'So it's easy to change when/if it gets changed officially.
+    Dim OpenRCTDLLName As String = ".\OpenRCT2\openrct2.dll"
 
     Dim LauncherVersion As Integer = "1" 'The Version of the launcher, so we can update the launcher as well.
 
@@ -32,11 +32,17 @@ Public Class frmLauncher
     Private Sub GetRemoteVer()
         If My.Computer.Network.IsAvailable = True Then
             Try 'If the computer has a network connection but no internet, we want to avoid crashing.
-                Dim WC As New WebClient
-                RemoteVer = WC.DownloadString(VerURL)       'Later on I am going to have to do some additional processing
+                Dim WS As New WebClient
+                WS.DownloadFile(URL, Path.GetTempPath & "OpenRCT2Update.html")
+                Dim HTML As String = File.ReadAllText(Path.GetTempPath & "OpenRCT2Update.html")
+                Dim Stuff As ArrayList = ParseLinks(HTML)
+                For Each thing In Stuff
+                    If thing.ToString.ToLower.StartsWith("http://cdn.limetric.com/games/openrct2") And thing.ToString.ToLower.EndsWith(".zip") Then
+                        RemoteVer = thing
+                    End If
+                Next
             Catch ex As Exception                           'because I want to grab the Download URL at the same time.
-                'MsgBox(ex.ToString)
-                RemoteVer = Integer.MinValue
+
             End Try
         End If
         RemoteDone = True
@@ -46,7 +52,7 @@ Public Class frmLauncher
         Try
             Dim Reg As RegistryKey = (Registry.CurrentUser.CreateSubKey("OpenRCT2Launcher"))
             If Reg.GetValue("LocalVer") Is Nothing Then
-                Reg.SetValue("LocalVer", "0")
+                Reg.SetValue("LocalVer", "0", RegistryValueKind.String)
                 LocalVer = "0"
             Else
                 LocalVer = Reg.GetValue("LocalVer")
@@ -62,7 +68,7 @@ Public Class frmLauncher
     Private Sub tmrCheckIfDone_Tick(sender As Object, e As EventArgs) Handles tmrCheckIfDone.Tick
         If LocalDone = True And RemoteDone = True Then
             tmrCheckIfDone.Enabled = False
-            If RemoteVer > LocalVer Then
+            If RemoteVer = LocalVer = False Then
                 lblStatus.Text = "Update is Available"
             Else
                 lblStatus.Text = "Up to Date"
@@ -73,11 +79,13 @@ Public Class frmLauncher
     Private Sub cmdLaunchGame_Click(sender As Object, e As EventArgs) Handles cmdLaunchGame.Click
         If File.Exists(OpenRCTEXEName) And File.Exists(OpenRCTDLLName) Then
             Dim Launch As New ProcessStartInfo
-            Launch.WindowStyle = ProcessWindowStyle.Maximized
-            Launch.FileName = OpenRCTEXEName
+            Launch.WorkingDirectory = ".\OpenRCT2"
+            Launch.FileName = "OpenRCT2.exe"
             Process.Start(Launch)
             Close()
+
         Else
+            MsgBox("OpenRCT2 Not Installed or Not Found!")
             'Code that does a force update
         End If
     End Sub
@@ -88,14 +96,23 @@ Public Class frmLauncher
             WS.DownloadFile(URL, Path.GetTempPath & "OpenRCT2Update.html")
             Dim HTML As String = File.ReadAllText(Path.GetTempPath & "OpenRCT2Update.html")
             Dim Stuff As ArrayList = ParseLinks(HTML)
+            Dim RemoteURL As String = Nothing
             For Each thing In Stuff
                 If thing.ToString.ToLower.StartsWith("http://cdn.limetric.com/games/openrct2") And thing.ToString.ToLower.EndsWith(".zip") Then
                     WS.DownloadFile(thing, "./update.zip")
+                    RemoteURL = thing
                 End If
             Next
-            ZipFile.ExtractToDirectory("./update.zip", "./")
+            If Directory.Exists("OpenRCT2") Then
+
+                Directory.Delete("OpenRCT2", True)
+            End If
+            ZipFile.ExtractToDirectory("./update.zip", "./OpenRCT2")
             File.Delete("./update.zip")
+            Dim Reg As RegistryKey = (Registry.CurrentUser.CreateSubKey("OpenRCT2Launcher"))
+            Reg.SetValue("LocalVer", RemoteURL)
         Catch ex As Exception
+            MsgBox(ex.ToString)
         End Try
     End Sub
 
