0bf6307832786a2949bb64a020f164e076c0ec81
Floris Westerman <floriswesterman@msn.com>
2015-02-16 01:11:12 +0100

    Added a unit test for symlink copying and Added some docs for the FileActions

--- Launcher/ReparsePointLibrary/FileActions.cs
+++ Launcher/ReparsePointLibrary/FileActions.cs
@@ -3,6 +3,12 @@ using System.IO;
 
 namespace HelperLibrary {
     public class FileActions {
+        /// <summary>
+        /// Copies all contents of a directory to another directory
+        /// </summary>
+        /// <param name="sourcePath">The path where the files should be copied from</param>
+        /// <param name="targetPath">The path where the files should be copied to</param>
+        /// <returns>The number of duplicate filenames</returns>
         public static int CopyDirectory(string sourcePath, string targetPath) {
             var doubles = 0;
 
@@ -27,6 +33,10 @@ namespace HelperLibrary {
             return doubles;
         }
 
+        /// <summary>
+        /// Removes all contents of specified directory
+        /// </summary>
+        /// <param name="path">The path to the folder that should be emptied</param>
         public static void ClearDirectory(string path) {
             if (!Directory.Exists(path)) return;
 
