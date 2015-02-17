586ee8928df33be34d6c89e516bfdc95d1b4f917
IntelOrca <ted@brambles.org>
2015-02-17 13:09:55 +0000

    use new ini class

--- Launcher/Launcher/Config.vb
+++ Launcher/Launcher/Config.vb
@@ -1,4 +1,6 @@
-﻿'Stores the configuration of OpenRCT2
+﻿Imports HelperLibrary
+
+'Stores the configuration of OpenRCT2
 Public Class OpenRCT2Config
     Enum EnumScreenshotFormat
         BMP
@@ -80,7 +82,7 @@ Public Class OpenRCT2Config
     Public SoundQuality As EnumSoundQuality
     Public ForcedSoftwareBuffering As Boolean
 
-    Dim INIConfig As New INI
+    Dim INIConfig As New IniConfiguration
 
     Public Sub LoadDefault()
         PlayIntro = False
@@ -108,59 +110,42 @@ Public Class OpenRCT2Config
         Dim Value As String
         Dim Number As Integer
 
-        INIConfig.Clear()
-
         Try
-            INIConfig.Load(File)
+            INIConfig = New IniConfiguration(File)
         Catch ex As Exception
             Return
         End Try
 
-        Value = INIConfig.FindValue("general", "play_intro")
+        PlayIntro = INIConfig.GetPropertyBoolean("general", "play_intro")
 
-        Select Case Value
-            Case "true"
-                PlayIntro = True
-            Case "false"
-                PlayIntro = False
-        End Select
-
-        Value = INIConfig.FindValue("general", "screenshot_format")
-
-        Select Case Value
+        Select Case INIConfig.GetProperty("general", "screenshot_format", "PNG")
             Case "BMP"
                 ScreenshotFormat = EnumScreenshotFormat.BMP
             Case "PNG"
                 ScreenshotFormat = EnumScreenshotFormat.PNG
         End Select
 
-        Value = INIConfig.FindValue("general", "game_path")
+        Value = INIConfig.GetProperty("general", "game_path")
 
-        If Value <> Nothing Then
+        If Not String.IsNullOrEmpty(Value) Then
             GamePath = Value
         End If
 
-        Value = INIConfig.FindValue("general", "measurement_format")
-
-        Select Case Value
+        Select Case INIConfig.GetProperty("general", "measurement_format")
             Case "IMPERIAL"
                 MeasurementFormat = EnumMeasurementFormat.Imperial
             Case "METRIC"
                 MeasurementFormat = EnumMeasurementFormat.Metric
         End Select
 
-        Value = INIConfig.FindValue("general", "temperature_format")
-
-        Select Case Value
+        Select Case INIConfig.GetProperty("general", "temperature_format")
             Case "CELSIUS"
                 TemperatureFormat = EnumTemperatureFormat.Celsius
             Case "FAHRENHEIT"
                 TemperatureFormat = EnumTemperatureFormat.Fahrenheit
         End Select
 
-        Value = INIConfig.FindValue("general", "currency_format")
-
-        Select Case Value
+        Select Case INIConfig.GetProperty("general", "currency_format")
             Case "GBP"
                 CurrencyFormat = EnumCurrencyFormat.Pounds
             Case "USD"
@@ -193,74 +178,27 @@ Public Class OpenRCT2Config
                 CurrencyFormat = EnumCurrencyFormat.Euros
         End Select
 
-        Value = INIConfig.FindValue("general", "edge_scrolling")
+        EdgeScrolling = INIConfig.GetPropertyBoolean("general", "edge_scrolling", True)
 
-        Select Case Value
-            Case "true"
-                EdgeScrolling = True
-            Case "false"
-                EdgeScrolling = False
-        End Select
+        AlwaysShowGridlines = INIConfig.GetPropertyBoolean("general", "always_show_gridlines")
 
-        Value = INIConfig.FindValue("general", "always_show_gridlines")
+        LandscapeSmoothing = INIConfig.GetPropertyBoolean("general", "landscape_smoothing", True)
 
-        Select Case Value
-            Case "true"
-                AlwaysShowGridlines = True
-            Case "false"
-                AlwaysShowGridlines = False
-        End Select
+        ShowHeightAsUnits = INIConfig.GetPropertyBoolean("general", "show_height_as_units")
 
-        Value = INIConfig.FindValue("general", "landscape_smoothing")
+        SavePluginData = INIConfig.GetPropertyBoolean("general", "save_plugin_data")
 
-        Select Case Value
-            Case "true"
-                LandscapeSmoothing = True
-            Case "false"
-                LandscapeSmoothing = False
-        End Select
+        Number = INIConfig.GetPropertyInt32("general", "fullscreen_mode")
 
-        Value = INIConfig.FindValue("general", "show_height_as_units")
-
-        Select Case Value
-            Case "true"
-                ShowHeightAsUnits = True
-            Case "false"
-                ShowHeightAsUnits = False
-        End Select
-
-        Value = INIConfig.FindValue("general", "save_plugin_data")
-
-        Select Case Value
-            Case "true"
-                SavePluginData = True
-            Case "false"
-                SavePluginData = False
-        End Select
-
-        Value = INIConfig.FindValue("general", "fullscreen_mode")
-
-        If Int32.TryParse(Value, Number) Then
-            If Number >= 0 And Number <= 2 Then
-                FullscreenMode = Number
-            End If
+        If Number >= 0 And Number <= 2 Then
+            FullscreenMode = Number
         End If
 
-        Value = INIConfig.FindValue("general", "fullscreen_width")
+        FullscreenWidth = INIConfig.GetPropertyInt32("general", "fullscreen_width", -1)
 
-        If Int32.TryParse(Value, Number) Then
-            FullscreenWidth = Number
-        End If
+        FullscreenHeight = INIConfig.GetPropertyInt32("general", "fullscreen_height", -1)
 
-        Value = INIConfig.FindValue("general", "fullscreen_height")
-
-        If Int32.TryParse(Value, Number) Then
-            FullscreenHeight = Number
-        End If
-
-        Value = INIConfig.FindValue("general", "language")
-
-        Select Case Value
+        Select Case INIConfig.GetProperty("general", "language")
             Case "en-GB"
                 Language = EnumLanguage.EnglishUK
             Case "en-US"
@@ -281,17 +219,13 @@ Public Class OpenRCT2Config
                 Language = EnumLanguage.Swedish
         End Select
 
-        Value = INIConfig.FindValue("general", "title_music")
+        Number = INIConfig.GetPropertyInt32("sound", "title_music", 2)
 
-        If Int32.TryParse(Value, Number) Then
-            If Number >= 0 And Number <= 2 Then
-                TitleMusic = Number
-            End If
+        If Number >= 0 And Number <= 2 Then
+            TitleMusic = Number
         End If
 
-        Value = INIConfig.FindValue("sound", "sound_quality")
-
-        Select Case Value
+        Select Case INIConfig.GetProperty("sound", "sound_quality", 2)
             Case "low"
                 SoundQuality = EnumSoundQuality.Low
             Case "medium"
@@ -300,141 +234,134 @@ Public Class OpenRCT2Config
                 SoundQuality = EnumSoundQuality.High
         End Select
 
-        Value = INIConfig.FindValue("sound", "forced_software_buffering")
-
-        Select Case Value
-            Case "true"
-                ForcedSoftwareBuffering = True
-            Case "false"
-                ForcedSoftwareBuffering = False
-        End Select
+        ForcedSoftwareBuffering = INIConfig.GetPropertyBoolean("sound", "forced_software_buffering")
     End Sub
 
     Public Sub SaveINI(File As String)
         If ConfirmationPrompt Then
-            INIConfig.SetValue("general", "confirmation_prompt", "true")
+            INIConfig.SetProperty("general", "confirmation_prompt", "true")
         Else
-            INIConfig.SetValue("general", "confirmation_prompt", "false")
+            INIConfig.SetProperty("general", "confirmation_prompt", "false")
         End If
 
         Select Case ScreenshotFormat
             Case EnumScreenshotFormat.BMP
-                INIConfig.SetValue("general", "screenshot_format", "BMP")
+                INIConfig.SetProperty("general", "screenshot_format", "BMP")
             Case EnumScreenshotFormat.PNG
-                INIConfig.SetValue("general", "screenshot_format", "PNG")
+                INIConfig.SetProperty("general", "screenshot_format", "PNG")
         End Select
 
-        INIConfig.SetValue("general", "game_path", GamePath)
+        INIConfig.SetProperty("general", "game_path", GamePath)
 
         Select Case MeasurementFormat
             Case EnumMeasurementFormat.Imperial
-                INIConfig.SetValue("general", "measurement_format", "IMPERIAL")
+                INIConfig.SetProperty("general", "measurement_format", "IMPERIAL")
             Case EnumMeasurementFormat.Metric
-                INIConfig.SetValue("general", "measurement_format", "METRIC")
+                INIConfig.SetProperty("general", "measurement_format", "METRIC")
         End Select
 
         Select Case TemperatureFormat
             Case EnumTemperatureFormat.Celsius
-                INIConfig.SetValue("general", "temperature_format", "CELSIUS")
+                INIConfig.SetProperty("general", "temperature_format", "CELSIUS")
             Case EnumTemperatureFormat.Fahrenheit
-                INIConfig.SetValue("general", "temperature_format", "FAHRENHEIT")
+                INIConfig.SetProperty("general", "temperature_format", "FAHRENHEIT")
         End Select
 
         Select Case CurrencyFormat
             Case EnumCurrencyFormat.Pounds
-                INIConfig.SetValue("general", "currency_format", "GBP")
+                INIConfig.SetProperty("general", "currency_format", "GBP")
             Case EnumCurrencyFormat.Dollars
-                INIConfig.SetValue("general", "currency_format", "USD")
+                INIConfig.SetProperty("general", "currency_format", "USD")
             Case EnumCurrencyFormat.Franc
-                INIConfig.SetValue("general", "currency_format", "FRF")
+                INIConfig.SetProperty("general", "currency_format", "FRF")
             Case EnumCurrencyFormat.Deutschmark
-                INIConfig.SetValue("general", "currency_format", "DEM")
+                INIConfig.SetProperty("general", "currency_format", "DEM")
             Case EnumCurrencyFormat.Yen
-                INIConfig.SetValue("general", "currency_format", "YEN")
+                INIConfig.SetProperty("general", "currency_format", "YEN")
             Case EnumCurrencyFormat.Peseta
-                INIConfig.SetValue("general", "currency_format", "ESP")
+                INIConfig.SetProperty("general", "currency_format", "ESP")
             Case EnumCurrencyFormat.Lira
-                INIConfig.SetValue("general", "currency_format", "ITL")
+                INIConfig.SetProperty("general", "currency_format", "ITL")
             Case EnumCurrencyFormat.Guilders
-                INIConfig.SetValue("general", "currency_format", "NLG")
+                INIConfig.SetProperty("general", "currency_format", "NLG")
             Case EnumCurrencyFormat.Krona
-                INIConfig.SetValue("general", "currency_format", "NOK")
+                INIConfig.SetProperty("general", "currency_format", "NOK")
             Case EnumCurrencyFormat.Euros
-                INIConfig.SetValue("general", "currency_format", "EUR")
+                INIConfig.SetProperty("general", "currency_format", "EUR")
         End Select
 
         If EdgeScrolling Then
-            INIConfig.SetValue("general", "edge_scrolling", "true")
+            INIConfig.SetProperty("general", "edge_scrolling", "true")
         Else
-            INIConfig.SetValue("general", "edge_scrolling", "false")
+            INIConfig.SetProperty("general", "edge_scrolling", "false")
         End If
 
         If AlwaysShowGridlines Then
-            INIConfig.SetValue("general", "always_show_gridlines", "true")
+            INIConfig.SetProperty("general", "always_show_gridlines", "true")
         Else
-            INIConfig.SetValue("general", "always_show_gridlines", "false")
+            INIConfig.SetProperty("general", "always_show_gridlines", "false")
         End If
 
         If LandscapeSmoothing Then
-            INIConfig.SetValue("general", "landscape_smoothing", "true")
+            INIConfig.SetProperty("general", "landscape_smoothing", "true")
         Else
-            INIConfig.SetValue("general", "landscape_smoothing", "false")
+            INIConfig.SetProperty("general", "landscape_smoothing", "false")
         End If
 
         If ShowHeightAsUnits Then
-            INIConfig.SetValue("general", "show_height_as_units", "true")
+            INIConfig.SetProperty("general", "show_height_as_units", "true")
         Else
-            INIConfig.SetValue("general", "show_height_as_units", "false")
+            INIConfig.SetProperty("general", "show_height_as_units", "false")
         End If
 
         If SavePluginData Then
-            INIConfig.SetValue("general", "save_plugin_data", "true")
+            INIConfig.SetProperty("general", "save_plugin_data", "true")
         Else
-            INIConfig.SetValue("general", "save_plugin_data", "false")
+            INIConfig.SetProperty("general", "save_plugin_data", "false")
         End If
 
-        INIConfig.SetValue("general", "fullscreen_mode", FullscreenMode)
+        INIConfig.SetProperty("general", "fullscreen_mode", FullscreenMode)
 
-        INIConfig.SetValue("general", "fullscreen_width", FullscreenWidth.ToString())
+        INIConfig.SetProperty("general", "fullscreen_width", FullscreenWidth.ToString())
 
-        INIConfig.SetValue("general", "fullscreen_height", FullscreenHeight.ToString())
+        INIConfig.SetProperty("general", "fullscreen_height", FullscreenHeight.ToString())
 
         Select Case Language
             Case EnumLanguage.EnglishUK
-                INIConfig.SetValue("general", "language", "en-GB")
+                INIConfig.SetProperty("general", "language", "en-GB")
             Case EnumLanguage.EnglishUS
-                INIConfig.SetValue("general", "language", "en-US")
+                INIConfig.SetProperty("general", "language", "en-US")
             Case EnumLanguage.German
-                INIConfig.SetValue("general", "language", "de-DE")
+                INIConfig.SetProperty("general", "language", "de-DE")
             Case EnumLanguage.Dutch
-                INIConfig.SetValue("general", "language", "nl-NL")
+                INIConfig.SetProperty("general", "language", "nl-NL")
             Case EnumLanguage.French
-                INIConfig.SetValue("general", "language", "fr-FR")
+                INIConfig.SetProperty("general", "language", "fr-FR")
             Case EnumLanguage.Hungarian
-                INIConfig.SetValue("general", "language", "hu-HU")
+                INIConfig.SetProperty("general", "language", "hu-HU")
             Case EnumLanguage.Polish
-                INIConfig.SetValue("general", "language", "pl-PL")
+                INIConfig.SetProperty("general", "language", "pl-PL")
             Case EnumLanguage.Spanish
-                INIConfig.SetValue("general", "language", "es-ES")
+                INIConfig.SetProperty("general", "language", "es-ES")
             Case EnumLanguage.Swedish
-                INIConfig.SetValue("general", "language", "sv-SE")
+                INIConfig.SetProperty("general", "language", "sv-SE")
         End Select
 
-        INIConfig.SetValue("general", "title_music", TitleMusic)
+        INIConfig.SetProperty("sound", "title_music", TitleMusic)
 
         Select Case SoundQuality
             Case EnumSoundQuality.Low
-                INIConfig.SetValue("sound", "sound_quality", "low")
+                INIConfig.SetProperty("sound", "sound_quality", "low")
             Case EnumSoundQuality.Medium
-                INIConfig.SetValue("sound", "sound_quality", "medium")
+                INIConfig.SetProperty("sound", "sound_quality", "medium")
             Case EnumSoundQuality.High
-                INIConfig.SetValue("sound", "sound_quality", "high")
+                INIConfig.SetProperty("sound", "sound_quality", "high")
         End Select
 
         If ForcedSoftwareBuffering Then
-            INIConfig.SetValue("sound", "forced_software_buffering", "true")
+            INIConfig.SetProperty("sound", "forced_software_buffering", "true")
         Else
-            INIConfig.SetValue("sound", "forced_software_buffering", "false")
+            INIConfig.SetProperty("sound", "forced_software_buffering", "false")
         End If
 
         Try
