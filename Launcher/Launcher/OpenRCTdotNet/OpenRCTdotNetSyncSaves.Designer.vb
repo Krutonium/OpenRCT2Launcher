e00b1667dce2f559d2e2b2cb312f4cf859b62637
Runelaenen <runelaenen@hotmail.com>
2015-03-29 21:40:55 +0200

    Sync Saves
    It works, but it should be tested some more I guess
    - Download synced saves if not present or older local version
    - Upload local saved if server is older or doesnt exist
    - Changed some layout stuff of the Configuration form

---
+++ Launcher/Launcher/OpenRCTdotNet/OpenRCTdotNetSyncSaves.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class OpenRCTdotNetSyncSaves
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdSync = New System.Windows.Forms.Button()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cmdSync
+        '
+        Me.cmdSync.Location = New System.Drawing.Point(111, 124)
+        Me.cmdSync.Name = "cmdSync"
+        Me.cmdSync.Size = New System.Drawing.Size(165, 27)
+        Me.cmdSync.TabIndex = 0
+        Me.cmdSync.Text = "Sync"
+        Me.cmdSync.UseVisualStyleBackColor = True
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(12, 9)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(39, 13)
+        Me.lblStatus.TabIndex = 1
+        Me.lblStatus.Text = "Label1"
+        '
+        'OpenRCTdotNetSyncSaves
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(398, 163)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.cmdSync)
+        Me.Name = "OpenRCTdotNetSyncSaves"
+        Me.Text = "Save Sync"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdSync As System.Windows.Forms.Button
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+End Class
