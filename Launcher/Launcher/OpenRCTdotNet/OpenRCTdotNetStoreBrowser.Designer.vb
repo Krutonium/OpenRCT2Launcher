cf983c385f6b75fe0a38a082ebba632dd525f899
Rune Laenen <runelaenen@hotmail.com>
2015-05-05 23:25:33 +0200

    Update OpenRCTdotNetStoreBrowser.Designer.vb

--- Launcher/Launcher/OpenRCTdotNet/OpenRCTdotNetStoreBrowser.Designer.vb
+++ Launcher/Launcher/OpenRCTdotNet/OpenRCTdotNetStoreBrowser.Designer.vb
@@ -1,7 +1,7 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class OpenRCTdotNetStoreBrowser
     Inherits System.Windows.Forms.Form
-
+ 
     'Form overrides dispose to clean up the component list.
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
